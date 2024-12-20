16ee466d298faf209935e0f98949cba514d1a99b
Scott Williams <scott.rw93@gmail.com>
2024-12-20 21:50:31 +0000

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "boolder-rails-680022408237.us-central1.run.app"
+
 end
